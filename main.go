97cef6862475f494c3566959baa8e831e383dc1b
DukeChiu <duke00360753@gmail.com>
2021-11-18 12:19:20 +0800

    initial simple auth

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,15 +16,91 @@ type IndexData struct {
 	Content string
 }
 
-func test(c *gin.Context) {
-	data := new(IndexData)
-	data.Title = "首頁"
-	data.Content = "我的第一個首頁"
-	c.HTML(http.StatusOK, "index.html", data)
+const (
+	USERNAME = "duke"
+	PASSWORD = "12345678"
+	NETWORK  = "tcp"
+	SERVER   = "127.0.0.1"
+	PORT     = 3307
+	DATABASE = "localduke"
+)
+
+// func test(c *gin.Context) {
+// 	data := new(IndexData)
+// 	data.Title = "首頁"
+// 	data.Content = "我的第一個首頁"
+// 	c.HTML(http.StatusOK, "index.html", data)
+// }
+
+func LoginPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
+func LoginAuth(c *gin.Context) {
+	var (
+		username string
+		password string
+	)
+	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
+		username = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("必須輸入使用者名稱"),
+		})
+		return
+	}
+	if in, isExist := c.GetPostForm("password"); isExist && in != "" {
+		password = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("必須輸入密碼名稱"),
+		})
+		return
+	}
+	if err := Auth(username, password); err == nil {
+		c.HTML(http.StatusOK, "login.html", gin.H{
+			"success": "登入成功",
+		})
+		return
+	} else {
+		c.HTML(http.StatusUnauthorized, "login.html", gin.H{
+			"error": err,
+		})
+		return
+	}
+}
+func CreateTable(db *sql.DB) error {
+	sql := `CREATE TABLE IF NOT EXISTS users(
+	id INT(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        username VARCHAR(64),
+        password VARCHAR(64)
+	); `
+
+	if _, err := db.Exec(sql); err != nil {
+		fmt.Println("建立 Table 發生錯誤:", err)
+		return err
+	}
+	fmt.Println("建立 Table 成功！")
+	return nil
 }
 func main() {
+	conn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s", USERNAME, PASSWORD, NETWORK, SERVER, PORT, DATABASE)
+	db, err := sql.Open("mysql", conn)
+	if err != nil {
+		fmt.Println("開啟 MySQL 連線發生錯誤，原因為：", err)
+		return
+	}
+	if err := db.Ping(); err != nil {
+		fmt.Println("資料庫連線錯誤，原因為：", err.Error())
+		return
+	}
+	defer db.Close()
+
+	fmt.Println(conn)
+	CreateTable(db)
 	server := gin.Default()
-	server.LoadHTMLGlob("template/*")
-	server.GET("/", test)
+	server.LoadHTMLGlob("template/html/*")
+	server.Static("/assets", "./template/assets")
+	server.GET("/login", LoginPage)
+	server.POST("/login", LoginAuth)
 	server.Run(":8888")
 }
