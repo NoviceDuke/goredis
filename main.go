4c4e8f93c532a8687cd2e6bfbeab0e4a7a712d99
DukeChiu <duke00360753@gmail.com>
2021-11-22 15:35:50 +0800

    add simple chat room

--- main.go
+++ main.go
@@ -1,134 +1,54 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
-	"fmt"
-	"log"
+	"encoding/json"
 	"net/http"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
+	"gopkg.in/olahol/melody.v1"
 )
 
-type IndexData struct {
-	Title   string
-	Content string
+type Message struct {
+	Event   string `json:"event"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
 }
 
-const (
-	USERNAME = "duke"
-	PASSWORD = "12345678"
-	NETWORK  = "tcp"
-	SERVER   = "127.0.0.1"
-	PORT     = 3307
-	DATABASE = "testdb"
-)
-
-// func test(c *gin.Context) {
-// 	data := new(IndexData)
-// 	data.Title = "首頁"
-// 	data.Content = "我的第一個首頁"
-// 	c.HTML(http.StatusOK, "index.html", data)
-// }
-
-func LoginPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "login.html", nil)
-}
-func LoginAuth(c *gin.Context) {
-	var (
-		username string
-		password string
-	)
-	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
-		username = in
-	} else {
-		c.HTML(http.StatusBadRequest, "login.html", gin.H{
-			"error": errors.New("必須輸入使用者名稱"),
-		})
-		return
-	}
-	if in, isExist := c.GetPostForm("password"); isExist && in != "" {
-		password = in
-	} else {
-		c.HTML(http.StatusBadRequest, "login.html", gin.H{
-			"error": errors.New("必須輸入密碼名稱"),
-		})
-		return
-	}
-	if err := Auth(username, password); err == nil {
-		c.HTML(http.StatusOK, "login.html", gin.H{
-			"success": "登入成功",
-		})
-		return
-	} else {
-		c.HTML(http.StatusUnauthorized, "login.html", gin.H{
-			"error": err,
-		})
-		return
+func NewMessage(event, name, content string) *Message {
+	return &Message{
+		Event:   event,
+		Name:    name,
+		Content: content,
 	}
 }
-func CreateTable(db *sql.DB) error {
-	sql := `CREATE TABLE IF NOT EXISTS users(
-	id INT(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-        username VARCHAR(64),
-        password VARCHAR(64)
-	); `
-
-	if _, err := db.Exec(sql); err != nil {
-		fmt.Println("建立 Table 發生錯誤:", err)
-		return err
-	}
-	fmt.Println("建立 Table 成功！")
-	return nil
-}
-
-type User struct {
-	ID       int64  `json:"id" gorm:"primary_key;auto_increase'"`
-	Username string `json:"username"`
-	Password string `json:""`
+func (m *Message) GetByteMessage() []byte {
+	result, _ := json.Marshal(m)
+	return result
 }
 
 func main() {
-	viper.SetConfigName("app")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./config")
-	viper.SetDefault("application.port", 8888)
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic("Read project err , cause :" + err.Error())
-	}
-	fmt.Println("application port = " + viper.GetString("application.port"))
-	conn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s", USERNAME, PASSWORD, NETWORK, SERVER, PORT, DATABASE)
-	db, err := gorm.Open(mysql.Open(conn), &gorm.Config{})
-	if err != nil {
-		panic("使用 gorm 連線 DB 發生錯誤，原因為 " + err.Error())
-	}
-	if err := db.AutoMigrate(new(User)); err != nil {
-		panic("資料庫 Migrate 失敗，原因為 " + err.Error())
-	}
-	user := &User{
-		Username: "test",
-		Password: "test",
-	}
-	if err := CreateUser(db, user); err != nil {
-		panic("資料庫 Migrate 失敗，原因為 " + err.Error())
-	}
-	if user, err := FindUser(db, 1); err == nil {
-		log.Println("查詢到 User 為 ", user)
-	} else {
-		panic("查詢 user 失敗，原因為 " + err.Error())
-	}
-}
-func CreateUser(db *gorm.DB, user *User) error {
-	return db.Create(user).Error
-}
-func FindUser(db *gorm.DB, id int64) (*User, error) {
-	user := new(User)
-	user.ID = id
-	err := db.First(&user).Error
-	return user, err
+	r := gin.Default()
+	m := melody.New()
+	r.LoadHTMLGlob("template/html/*")
+	r.Static("/assets", "./template/assets")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+	m.HandleConnect(func(session *melody.Session) {
+		id := session.Request.URL.Query().Get("id")
+		m.Broadcast(NewMessage("other", id, "加入聊天室").GetByteMessage())
+	})
+	m.HandleClose(func(session *melody.Session, i int, s2 string) error {
+		id := session.Request.URL.Query().Get("id")
+		m.Broadcast(NewMessage("other", id, "離開聊天室").GetByteMessage())
+		return nil
+	})
+
+	r.Run(":8888")
 }
